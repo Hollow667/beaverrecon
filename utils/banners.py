8a3016addb6f3321a852b103c057eedab4cf3d24
cat-linux <67713854+cat-linux@users.noreply.github.com>
2020-07-17 13:42:27 -0700

    Add files via upload

--- utils/banners.py
+++ utils/banners.py
@@ -4,98 +4,80 @@ from utils.update import *
 
 def ipmenu(themec, target):
     print(f'''{reset}
-{bold}Current Target: {themec}{target}{reset}
- _____________________________________________
-| {themec}1{reset} | {bold}GeoIP{reset}         | {themec}Geo Locate A I.P.{reset}       |
-| {themec}2{reset} | {bold}ThatsThem{reset}     | {themec}Lookup On ThatsThem.com{reset} |
-| {themec}3{reset} | {bold}Change Target{reset} | {themec}Change Current Target{reset}   |
-| {themec}0{reset} | {bold}Go Back{reset}       |_________________________|
-|___|_______________|
+ {bold}Current Target: {themec}{target}{reset}
+╭───┬───────────────┬─────────────────────────╮
+│ {themec}1{reset} │ {bold}GeoIP{reset}         │ {themec}Geo Locate A I.P.{reset}       │
+│ {themec}2{reset} │ {bold}ThatsThem{reset}     │ {themec}Lookup On ThatsThem.com{reset} │
+│ {themec}3{reset} │ {bold}Change Target{reset} │ {themec}Change Current Target{reset}   │
+│ {themec}0{reset} │ {bold}Go Back{reset}       ├─────────────────────────╯
+╰───┴───────────────╯
     ''')
 
 def usernamemenu(themec, target):
     print(f'''{reset}
-{bold}Current Target: {themec}{target}{reset}
- ________________________________________________________________________
-| {themec}1{reset} | {bold}scylla{reset}        | {themec}Data Breach Lookup(Shows Leaked Info){reset}              |
-| {themec}2{reset} | {bold}instagram{reset}     | {themec}Get The Partial Email or Phone On A Instagram User{reset} |
-| {themec}3{reset} | {bold}change target{reset} | {themec}Change Current Target{reset}                              |
-| {themec}0{reset} | {bold}Go Back{reset}       |____________________________________________________|
-|___|_______________|
+ {bold}Current Target: {themec}{target}{reset}
+╭───┬───────────────┬────────────────────────────────────────────────────╮
+│ {themec}1{reset} │ {bold}scylla{reset}        │ {themec}Data Breach Lookup(Shows Leaked Info){reset}              │
+│ {themec}2{reset} │ {bold}instagram{reset}     │ {themec}Get The Partial Email or Phone On A Instagram User{reset} │
+│ {themec}3{reset} │ {bold}change target{reset} │ {themec}Change Current Target{reset}                              │
+│ {themec}0{reset} │ {bold}Go Back{reset}       ├────────────────────────────────────────────────────╯
+╰───┴───────────────╯
     ''')
 
 def emailmenu(themec, target):
     print(f'''{reset}
-{bold}Current Target: {themec}{target}{reset}
- ___________________________________________________________
-| {themec}1{reset} | {bold}EmailRep{reset}      | {themec}Email Reputation Lookup{reset}               |
-| {themec}2{reset} | {bold}Scylla{reset}        | {themec}Data Breach Lookup(shows leaked info){reset} |
-| {themec}3{reset} | {bold}Pwndb2{reset}        | {themec}Data Breach Lookup(shows leaked info){reset} |
-| {themec}4{reset} | {bold}HIBP{reset}          | {themec}Data Breach Lookup(only shows leaks){reset}  |
-| {themec}5{reset} | {bold}Change Target{reset} | {themec}Change Current Target{reset}                 |
-| {themec}0{reset} | {bold}Go Back{reset}       |_______________________________________|
-|___|_______________|
+ {bold}Current Target: {themec}{target}{reset}
+╭───┬───────────────┬───────────────────────────────────────╮
+│ {themec}1{reset} │ {bold}EmailRep{reset}      │ {themec}Email Reputation Lookup{reset}               │
+│ {themec}2{reset} │ {bold}Scylla{reset}        │ {themec}Data Breach Lookup(shows leaked info){reset} │
+│ {themec}3{reset} │ {bold}Pwndb2{reset}        │ {themec}Data Breach Lookup(shows leaked info){reset} │
+│ {themec}4{reset} │ {bold}HIBP{reset}          │ {themec}Data Breach Lookup(only shows leaks){reset}  │
+│ {themec}5{reset} │ {bold}Change Target{reset} │ {themec}Change Current Target{reset}                 │
+│ {themec}0{reset} │ {bold}Go Back{reset}       ├───────────────────────────────────────╯
+╰───┴───────────────╯
     ''')
 
 
 def settingsmenu(themec):
     print(f'''{reset}
- ______________________
-| {themec}1{reset} | {themec}Color Menu{reset}       |
-| {themec}2{reset} | {themec}Update Api Keys{reset}  |
-| {themec}3{reset} | {themec}Check For Update{reset} |
-| {themec}0{reset} | {themec}Go Back{reset}          |
-|___|__________________|
+╭───┬──────────────────╮
+│ {themec}1{reset} │ {themec}Color Menu{reset}       │
+│ {themec}2{reset} │ {themec}Update Api Keys{reset}  │
+│ {themec}3{reset} │ {themec}Check For Update{reset} │
+│ {themec}0{reset} │ {themec}Go Back{reset}          │
+╰───┴──────────────────╯
     ''')
 
 def banner(themec, version):
     return (f'''{themec}
- ______
-|   __ \.-----.---.-.--.--.-----.----.
-|   __ <|  -__|  _  |  |  |  -__|   _|
-|______/|_____|___._|\___/|_____|__|
- ______
-|   __ \.-----.----.-----.-----.
-|      <|  -__|  __|  _  |     |
-|___|__||_____|____|_____|__|__|
-
- Made By {reset}catlinux{themec} with love <3{reset}
-
- {themec}{checkversion(version)}{reset}
- ____________________
-| {themec}Version{reset}: {version}     |
-|____________________|
-| {themec}1{reset} | {themec}IP{reset}             |
-| {themec}2{reset} | {themec}Username{reset}       |
-| {themec}3{reset} | {themec}Email{reset}          |
-| {themec}4{reset} | {themec}Phone{reset}          |
-| {themec}5{reset} | {themec}Hash Decrypt{reset}   |
-| {themec}6{reset} | {themec}Info{reset}           |
-| {themec}7{reset} | {themec}Settings{reset}       |
-| {themec}0{reset} | {themec}Exit{reset}           |
-|___|________________|
-''' + reset)
+ {reset}╭──────────┬───────┬────────╮ {themec}Version{reset}: {version}
+ {reset}│  {themec}B E A V E R   R E C O N  {reset}│ {themec}{checkversion(version)}{reset}
+ {reset}╰──────────╰───────╯────────╯ {themec}Coded By{reset}: catlinux{themec}{reset}
+    ╭────────────────────╮
+    │       {reset}Options      │ 
+    ├───┬────────────────┤
+    │ {themec}1{reset} │ {themec}IP{reset}             │
+    │ {themec}2{reset} │ {themec}Username{reset}       │
+    │ {themec}3{reset} │ {themec}Email{reset}          │
+    │ {themec}4{reset} │ {themec}Phone{reset}          │
+    │ {themec}5{reset} │ {themec}Hash Decrypt{reset}   │
+    │ {themec}6{reset} │ {themec}Info{reset}           │
+    │ {themec}7{reset} │ {themec}Settings{reset}       │
+    │ {themec}0{reset} │ {themec}Exit{reset}           │
+    ╰───┴────────────────╯''' + reset)
 
 def colormenu():
-    print(f'''{reset}
- __________________________
-|    color selection       |
-|__________________________|
-| {black}black{reset}      | {bgblack}bgblack{reset}     |
-| {red}red{reset}        | {bgred}bgred{reset}       |
-| {green}green{reset}      | {bggreen}bggreen{reset}     |
-| {orange}orange{reset}     | {bgorange}bgorange{reset}    |
-| {blue}blue{reset}       | {bgblue}bgblue{reset}      |
-| {purple}purple{reset}     | {bgpurple}bgpurple{reset}    |
-| {cyan}cyan{reset}       | {bgcyan}bgcyan{reset}      |
-| {lightgrey}lightgrey{reset}  | {bglightgrey}bglightgrey{reset} |
-| {darkgrey}darkgrey{reset}   |_____________| 
-| {lightred}lightred{reset}   |
-| {lightgreen}lightgreen{reset} |
-| {yellow}yellow{reset}     |
-| {lightblue}lightblue{reset}  |
-| {pink}pink{reset}       |
-| {lightcyan}lightcyan{reset}  |
-|____________|
+    print(f'''
+╭──────────────────────────╮
+│    color selection       │
+├────────────┬─────────────┤
+│ {black}black{reset}      │ {darkgrey}darkgrey{reset}    │
+│ {red}red{reset}        │ {lightred}lightred{reset}    │
+│ {green}green{reset}      │ {lightgreen}lightgreen{reset}  │
+│ {orange}orange{reset}     │ {yellow}yellow{reset}      │
+│ {blue}blue{reset}       │ {lightblue}lightblue{reset}   │
+│ {purple}purple{reset}     │ {pink}pink{reset}        │
+│ {cyan}cyan{reset}       │  {lightcyan}lightcyan{reset}  │
+│ {lightgrey}lightgrey{reset}  ├─────────────╯
+╰────────────╯
 ''')
-
